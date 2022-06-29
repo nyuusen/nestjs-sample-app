d271cf894873e05a94d9cd8833c75e78387ed39c
tom-illy <pppike.0131@gmail.com>
2022-06-29 14:46:11 +0900

    service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { CatsController } from 'src/cats/cats.controller';
+import { CatsService } from 'src/cats/cats.service';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DogsController } from './dogs/dogs.controller';
@@ -7,6 +8,6 @@ import { DogsController } from './dogs/dogs.controller';
 @Module({
   imports: [],
   controllers: [AppController, DogsController, CatsController],
-  providers: [AppService],
+  providers: [AppService, CatsService],
 })
 export class AppModule {}
--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -12,18 +12,21 @@ import {
   CreateCatDto,
   UpdateCatDto,
   ListAllEntities,
-} from 'src/cats/create-cat.dto';
+} from 'src/cats/dto/create-cat.dto';
+import { CatsService } from './cats.service';
 
 @Controller('cats')
 export class CatsController {
+  constructor(private catsService: CatsService) {}
+
   @Post()
   create(@Body() createCatDto: CreateCatDto) {
-    return 'This action adds a new cat';
+    this.catsService.create(createCatDto);
   }
 
   @Get()
   findAll(@Query() query: ListAllEntities) {
-    return `This action returns all cats (limit: ${query.limit} items)`;
+    return this.catsService.findAll();
   }
 
   @Get(':id')

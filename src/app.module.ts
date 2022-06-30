21195f780e7153c92f359a642c40a3da7a1ffad1
tom-illy <pppike.0131@gmail.com>
2022-06-30 09:32:35 +0900

    authentication

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,14 @@ import { LoggerMiddleware } from 'src/middleware/logger.middleware';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DogsController } from './dogs/dogs.controller';
+import { AuthModule } from './auth/auth.module';
+import { UsersService } from './users/users.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [CatsModule],
+  imports: [CatsModule, AuthModule, UsersModule],
   controllers: [AppController, DogsController],
-  providers: [AppService],
+  providers: [AppService, UsersService],
 })
 export class AppModule implements NestModule {
   // カスタムミドルウェアの登録はconfigureで行う
